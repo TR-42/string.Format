23bf51efd883d6228567fa1b040951e1f97f3cb2
kfujita <kfujita@student.42tokyo.jp>
2022-12-15 14:38:57 +0900

    Create MyString.Format.cs

---
+++ string.Format/MyString.Format.cs
@@ -0,0 +1,47 @@
+using System;
+
+namespace TR
+{
+	public class MyString
+	{
+		public static string Format(string format, object arg0)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static string Format(string format, params object[] args)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static string Format(IFormatProvider provider, string format, object arg0)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static string Format(IFormatProvider provider, string format, params object[] args)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static string Format(string format, object arg0, object arg1)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static string Format(IFormatProvider provider, string format, object arg0, object arg1)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static string Format(string format, object arg0, object arg1, object arg2)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static string Format(IFormatProvider provider, string format, object arg0, object arg1, object arg2)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
