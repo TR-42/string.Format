df065ed24d7762cd1844a7f24bb211ba345aabd5
kfujita <kfujita@student.42tokyo.jp>
2022-12-16 15:31:23 +0900

    `MyStringFormatter`を使う形で実装

--- string.Format/MyString.Format.cs
+++ string.Format/MyString.Format.cs
@@ -6,42 +6,42 @@ namespace TR
 	{
 		public static string Format(string format, object arg0)
 		{
-			throw new NotImplementedException();
+			return new MyStringFormatter(format).Format(arg0);
 		}
 
 		public static string Format(string format, params object[] args)
 		{
-			throw new NotImplementedException();
+			return new MyStringFormatter(format).Format(args);
 		}
 
 		public static string Format(IFormatProvider provider, string format, object arg0)
 		{
-			throw new NotImplementedException();
+			return new MyStringFormatter(provider, format).Format(arg0);
 		}
 
 		public static string Format(IFormatProvider provider, string format, params object[] args)
 		{
-			throw new NotImplementedException();
+			return new MyStringFormatter(provider, format).Format(args);
 		}
 
 		public static string Format(string format, object arg0, object arg1)
 		{
-			throw new NotImplementedException();
+			return new MyStringFormatter(format).Format(arg0, arg1);
 		}
 
 		public static string Format(IFormatProvider provider, string format, object arg0, object arg1)
 		{
-			throw new NotImplementedException();
+			return new MyStringFormatter(provider, format).Format(arg0, arg1);
 		}
 
 		public static string Format(string format, object arg0, object arg1, object arg2)
 		{
-			throw new NotImplementedException();
+			return new MyStringFormatter(format).Format(arg0, arg1, arg2);
 		}
 
 		public static string Format(IFormatProvider provider, string format, object arg0, object arg1, object arg2)
 		{
-			throw new NotImplementedException();
+			return new MyStringFormatter(provider, format).Format(arg0, arg1, arg2);
 		}
 	}
 }
