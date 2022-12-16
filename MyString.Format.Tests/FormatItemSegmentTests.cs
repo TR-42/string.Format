6afe0ae7b4595fb2e92e2190b0ba3879ad44b948
kfujita <kfujita@student.42tokyo.jp>
2022-12-16 12:17:01 +0900

    Create FormatItemSegmentTests.cs

---
+++ MyString.Format.Tests/FormatItemSegmentTests.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace MyString.Format.Tests
+{
+	public class FormatItemSegmentTests
+	{
+		[TestCase(0, 16)]
+		[TestCase(0, 3)]
+		[TestCase(0, int.MaxValue)]
+		[TestCase(int.MaxValue - 3, 3)]
+		public void NormalCaseTest(int StartIndex, int Length)
+			=> Assert.That(
+				() => new TR.FormatItemSegment(StartIndex, Length),
+				Throws.Nothing
+			);
+
+		[TestCase(-1, 16)]
+		[TestCase(int.MinValue, 16)]
+		public void StartIndexOutOfRangeTests(int StartIndex, int Length)
+			=> Tester<ArgumentOutOfRangeException>(StartIndex, Length, "must be positive (Parameter 'StartIndex')");
+
+		[TestCase(0, -1)]
+		[TestCase(0, 0)]
+		[TestCase(0, 1)]
+		[TestCase(0, 2)]
+		public void LengthOutOfRangeTests(int StartIndex, int Length)
+			=> Tester<ArgumentOutOfRangeException>(StartIndex, Length, "must be same or more than 3 (Open Brace, Index Number, and Close Brace) (Parameter 'Length')");
+
+		[TestCase(1, int.MaxValue)]
+		[TestCase(int.MaxValue - 2, 3)]
+		[TestCase(int.MaxValue, int.MaxValue)]
+		public void OverflowTests(int StartIndex, int Length)
+			=> Tester<OverflowException>(StartIndex, Length, "`StartIndex + Length` will overflow with given values");
+
+		static void Tester<T>(int StartIndex, int Length, string message) where T : Exception
+			=> Assert.That(
+				() => new TR.FormatItemSegment(StartIndex, Length),
+				Throws.InstanceOf<T>().And.Message.EqualTo(message)
+			);
+	}
+}
