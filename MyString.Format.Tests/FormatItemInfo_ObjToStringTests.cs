2d5a812048ee1fc9dddc29ff6403088d9aa1608d
kfujita <kfujita@student.42tokyo.jp>
2022-12-16 14:33:24 +0900

    Create FormatItemInfo_ObjToStringTests.cs

---
+++ MyString.Format.Tests/FormatItemInfo_ObjToStringTests.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Globalization;
+
+namespace MyString.Format.Tests
+{
+	public class FormatItemInfo_ObjToStringTests
+	{
+		[TestCase("D", 123, "123")]
+		[TestCase("X", 123, "7B")]
+		[TestCase("#,000.0", 1234.51, "1,234.5")]
+		[TestCase("##,000.0##", 1234.51, "1,234.51")]
+		public void Test_WithoutFormatProvider(string formatString, object obj, string expected)
+			=> Assert.That(
+				new TR.FormatItemInfo(0, 0, formatString).ObjToString(obj, null),
+				Is.EqualTo(expected)
+			);
+
+		[TestCase("C", 123, "$123.00")]
+		[TestCase("N", 12345, "12,345.000")]
+		[TestCase("C", 1234.51, "$1,234.51")]
+		[TestCase("P", 0.123451, "12.345%")]
+		public void Test_WithFormatProvider(string formatString, object obj, string expected)
+			=> Assert.That(
+				new TR.FormatItemInfo(0, 0, formatString).ObjToString(obj, CultureInfo.CreateSpecificCulture("en-US")),
+				Is.EqualTo(expected)
+			);
+	}
+}
