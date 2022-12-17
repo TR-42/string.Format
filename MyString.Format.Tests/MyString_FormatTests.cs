5ef17ce3c5d0bdd591169cbc72b8a63731f65043
kfujita <kfujita@student.42tokyo.jp>
2022-12-17 12:13:07 +0900

    ロケールを固定

--- MyString.Format.Tests/MyString_FormatTests.cs
+++ MyString.Format.Tests/MyString_FormatTests.cs
@@ -1,9 +1,14 @@
+using System;
+using System.Globalization;
+
 using TR;
 
 namespace MyString.Format.Tests;
 
 public class MyString_FormatTests
 {
+	static readonly IFormatProvider formatProvider = CultureInfo.CreateSpecificCulture("en-US");
+
 	[TestCase("", 123, ExpectedResult = "")]
 	[TestCase("{{", 123, ExpectedResult = "{")]
 	[TestCase("}}", 123, ExpectedResult = "}")]
@@ -16,19 +21,19 @@ public class MyString_FormatTests
 	[TestCase("{0,5}", 123, ExpectedResult = "  123")]
 	[TestCase("{0,5:X}", 123, ExpectedResult = "   7B")]
 	public string FormatTest_OneArg(string format, object arg0)
-		=> new MyStringFormatter(format).Format(arg0);
+		=> new MyStringFormatter(formatProvider, format).Format(arg0);
 
 	[TestCase("{0} {1}", 123, 1.23, ExpectedResult = "123 1.23")]
 	[TestCase("{0,4}{1,5}", 123, 1.23, ExpectedResult = " 123 1.23")]
 	[TestCase("{0,4:X}{1,10:P}", 123, 1.23, ExpectedResult = "  7B  123.000%")]
 	public string FormatTest_TwoArg(string format, object arg0, object arg1)
-		=> new MyStringFormatter(format).Format(arg0, arg1);
+		=> new MyStringFormatter(formatProvider, format).Format(arg0, arg1);
 
 	[TestCase("{0} {1} {2}", 123, 1.23, 4.56, ExpectedResult = "123 1.23 4.56")]
 	public string FormatTest_ThreeArg(string format, object arg0, object arg1, object arg2)
-		=> new MyStringFormatter(format).Format(arg0, arg1, arg2);
+		=> new MyStringFormatter(formatProvider, format).Format(arg0, arg1, arg2);
 
 	[TestCase("{0} {1} {2} {3}", 123, 1.23, 4.56, 7.89, ExpectedResult = "123 1.23 4.56 7.89")]
 	public string FormatTest_FourArg(string format, object arg0, object arg1, object arg2, object arg3)
-		=> new MyStringFormatter(format).Format(new object[] { arg0, arg1, arg2, arg3 });
+		=> new MyStringFormatter(formatProvider, format).Format(new object[] { arg0, arg1, arg2, arg3 });
 }
